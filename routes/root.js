0df7529b63e2c2c00be2d22004dd1b2422835707
MzUgM <108896003+MzUgM@users.noreply.github.com>
2024-03-24 11:27:44 -0700

    fix: talk to self

--- routes/root.js
+++ routes/root.js
@@ -5,4 +5,19 @@ module.exports = async function (fastify, opts) {
   fastify.get('/example', async (request, reply) => {
     return { 'some-one': 'here', ah: 'fudge' }
   })
+
+  fastify.post('/example', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['name'],
+        properties: {
+          name: { type: 'string' }
+        }
+      }
+    }
+  }, async (request, reply) => {
+    const { name } = request.body
+    return { [name]: 'here', ah: 'fudge' }
+  })
 }
